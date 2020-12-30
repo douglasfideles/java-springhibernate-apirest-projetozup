2940eef46f7fc3c66af7db34a990f8e89c0423e9
Douglas Rodrigues Fideles <47896736+douglasfideles@users.noreply.github.com>
2020-12-29 22:04:27 -0300

    init

---
+++ registerbank/src/main/java/br/com/zup/registerbank/RegisterbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.registerbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterbankApplication.class, args);
+	}
+
+}
